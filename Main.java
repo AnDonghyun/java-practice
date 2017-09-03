5c2c1e5e672718076f5eb6ac3df8d97fa78b96a2
Donghyun An <miehdan@gmail.com>
2017-09-03 23:53:15 +0900

    Q290 - 144

--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        testIfA();
-    }
-    public static void testIfA() {
-        if (testIfB("true")) {
-            System.out.println("True");
-        } else {
-            System.out.println("Not true");
+    public static void main(String[] args) throws Exception{
+        // throw new Exception();
+        try{
+            // throw new Exception();
+        } catch(Exception e){
+            throw new Exception();
+        } finally {
+            System.out.println("FINALLY");
         }
     }
-    public static Boolean testIfB(String str) {
-        return Boolean.valueOf(str);
-    }
+    
 }
