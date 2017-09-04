4f92d44f828739001c28ff041973dbeaed4ff439
Donghyun An <miehdan@gmail.com>
2017-09-04 23:06:07 +0900

    Q290 - 176

---
+++ GenericB.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class GenericB <T extends Pet> {
+    public T foo;
+
+    public void setFoo(T foo) {
+        this.foo = foo;
+    }
+
+    public T getFoo() {
+        return foo;
+    }
+
+    public static void main(String[] args) {
+        GenericB<Cat> bar = new GenericB<Cat>();
+        bar.setFoo(new Cat());
+        Cat o = bar.getFoo();
+    }
+}
+
+interface Pet {}
+class Cat implements Pet {}
