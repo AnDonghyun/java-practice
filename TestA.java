1fe6ddb8c60133d6a5ffd3e8cb3fc7363be7f03a
Donghyun An <miehdan@gmail.com>
2017-09-05 22:08:55 +0900

    Q290 - 177

---
+++ TestA.java
@@ -0,0 +1,16 @@
+class TestException extends Exception { }
+class A {
+    public String sayHello(String name) throws TestException {
+        if(name == null) throw new TestException();
+        return "Hello " + name;
+    }
+}
+public class TestA {
+    public static void main(String[] args) {
+        try{
+           new A().sayHello("Aiko");
+       } catch (TestException te) {
+            System.out.println(te);
+       }
+   }
+}
