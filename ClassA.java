76fcea4ffa2d017538d8245f966c3caca8d2d0fb
Donghyun An <miehdan@gmail.com>
2017-09-10 20:29:35 +0900

    Q290 - 271

---
+++ ClassA.java
@@ -0,0 +1,28 @@
+public class ClassA {
+    public void methodA() {
+        ClassB classB = new ClassB();
+        classB.getValue();
+    }
+
+    public static void main(String[] args) {
+        ClassA a = new ClassA();
+        a.methodA();
+    }
+}
+
+class ClassB {
+    public ClassC classC;
+
+    public String getValue() {
+        return classC.getValue();
+    }
+}
+
+class ClassC {
+    public String value;
+
+    public String getValue() {
+        value = "ClassB";
+        return value;
+    }
+}
