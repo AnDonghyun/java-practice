16ed6964da93a62b6916b2bd72b6d86c4cc503ad
Donghyun An <miehdan@gmail.com>
2017-09-10 20:12:19 +0900

    Q290 - 264

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            args = null;
+            args[0] = "test";
+            System.out.println(args[0]);
+        } catch (Exception ex) {
+            System.out.println("Exception");
+        } catch (NullPointerException npe) {
+            System.out.println("NullPointerException");
+        }
+    }
+}
